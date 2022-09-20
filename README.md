39051796779d06cb5c9bf8464fbbae8f9678025e
Andrei Batomunkuev <andreibatomunkuev@gmail.com>
2022-09-20 01:24:52 -0400

    Project Started: Vectors

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# linearity
-Project "Linearity" - linear algebra in Python
+# Linearity - WIP
+Project "Linearity" - linear algebra in Python. This repository is for educational purposes to understand Linear Algebra through coding ^^ 
+
+## Features
+
+Vector initialization
+```python
+from linearity.core.engine import Vector
+
+# Initialize 1D vector 
+v = Vector([1,2,3])
+v
+
+# Output: Vector(data=[1, 2, 3])
+
+# Initialize 2D Vector
+t = Vector([[1,3,4], [3,6,8]])
+# Output:
+#[
+#  Vector(data=[1, 3, 4])
+#  Vector(data=[3, 6, 8])
+#]
+```
+
+Get the shape of the vector
+```python
+from linearity.core.engine import Vector
+
+# Initialize 1D vector 
+v = Vector([1,2,3])
+v.shape
+# Output: (3,)
+```
+
+## Log
+- 20 Sep 2022 - created Vector class, added 3 methods: __repr__, __len__, and defined a function which finds the shape of the vector
---
+++ linearity/core/engine.py
@@ -0,0 +1,45 @@
+from typing import Tuple
+
+class Module:
+    def find_shape(self, ndarray) -> Tuple[int,...]:
+        if isinstance(ndarray, list):
+            # More dimensions make a recursive call
+            outermost_size = len(ndarray)
+            row_shape = self.find_shape(ndarray[0])
+            return (outermost_size, *row_shape)
+        else:
+            # No more dimensions
+            return ()
+
+class Vector(Module):
+    """
+    Core data structure of Linearity: Vector
+
+    Algebraic definition
+    ====================
+    Vector - an ordered collection of some numeric data.
+
+    Geometric definition
+    ====================
+    Directed line segment which has specified beginning (head)
+    and end (tail). It has 2 main characteristics:
+    1. Length
+    2. Direction
+    """
+    def __init__(self, data):
+        """
+        Constructor
+
+        self: represents instance of a 'Vector' class
+        data: data to be stored
+        """
+        self.data = list(map(float, data)) if isinstance(data, int) else data
+        self.shape = self.find_shape(data)
+
+    def __repr__(self):
+        default_print = f"Vector(data={self.data})"
+        multiline_print = "[\n" + "\n".join(f"Vector(data={row})".rjust(len(f"Vector(data={row})") + 2) for row in self.data) + "\n]"
+        return multiline_print if len(self.shape) > 1 else default_print
+
+    def __len__(self):
+        return len(self.data)
